a38360e8053f3acf2eaca33ce0c15097045d9ff2
A.I <merrick@luois.me>
2017-09-08 16:40:51 +0800

    fix android import

--- android/src/main/java/me/youchai/reactnative/RNUmengAnalyticsModule.java
+++ android/src/main/java/me/youchai/reactnative/RNUmengAnalyticsModule.java
@@ -5,7 +5,12 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.umeng.analytics.MobclickAgent;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RNUmengAnalyticsModule extends ReactContextBaseJavaModule {
 
