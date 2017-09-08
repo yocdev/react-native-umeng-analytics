6d4977fe907f91363928601fe30dff593135dd8f
A.I <merrick@luois.me>
2017-09-08 16:25:22 +0800

    add android implementation

--- android/src/main/java/me/youchai/reactnative/RNUmengAnalyticsModule.java
+++ android/src/main/java/me/youchai/reactnative/RNUmengAnalyticsModule.java
@@ -5,18 +5,62 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.umeng.analytics.MobclickAgent;
 
 public class RNUmengAnalyticsModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNUmengAnalyticsModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNUmengAnalyticsModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNUmengAnalytics";
-  }
+    @Override
+    public String getName() {
+        return "RNUmengAnalytics";
+    }
+
+    @ReactMethod
+    public void start(String appKey, String channelId) {
+        MobclickAgent.UMAnalyticsConfig config =
+            new MobclickAgent.UMAnalyticsConfig(getReactApplicationContext(), appKey, channelId);
+        MobclickAgent.startWithConfigure(config);
+    }
+
+    @ReactMethod
+    public void onEvent(String eventId) {
+        MobclickAgent.onEvent(getCurrentActivity(), eventId);
+    }
+
+    @ReactMethod
+    public void onEventWithLabel(String eventId, String eventLabel) {
+        MobclickAgent.onEvent(getCurrentActivity(), eventId, eventLabel);
+    }
+
+    @ReactMethod
+    public void onEventWithParameters(String eventId, ReadableMap parameters) {
+        Map<String, String> map = new HashMap<>();
+        ReadableMapKeySetIterator iterator = parameters.keySetIterator();
+        while (iterator.hasNextKey()) {
+            String key = iterator.nextKey();
+            map.put(key, parameters.getString(key));
+        }
+        MobclickAgent.onEvent(getCurrentActivity(), eventId, map);
+    }
+
+    @ReactMethod
+    public void onPageBegin(String pageName) {
+        MobclickAgent.onEvent(getCurrentActivity(), pageName);
+    }
+
+    @ReactMethod
+    public void onPageEnd(String pageName) {
+        MobclickAgent.onEvent(getCurrentActivity(), pageName);
+    }
+
+    @ReactMethod
+    public void setDebugMode(boolean value) {
+        MobclickAgent.setDebugMode(value);
+    }
 }
