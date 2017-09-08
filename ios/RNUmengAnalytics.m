22749800f4fffd3ce3f6b4c26210401c18490b5c
A.I <merrick@luois.me>
2017-09-08 16:29:15 +0800

    add ios implementation

--- ios/RNUmengAnalytics.m
+++ ios/RNUmengAnalytics.m
@@ -2,12 +2,36 @@
 #import "RNUmengAnalytics.h"
 
 @implementation RNUmengAnalytics
+RCT_EXPORT_MODULE();
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
+RCT_EXPORT_METHOD(start:(NSString *)key channelId:(NSString *)channelId) {
+  UMConfigInstance.appKey = key;
+  UMConfigInstance.channelId = channelId;
+  [MobClick startWithConfigure:UMConfigInstance];
 }
-RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(onEvent:(NSString*)eventId) {
+  [MobClick event:eventId];
+}
+
+RCT_EXPORT_METHOD(onEventWithLabel:(NSString *)eventId label:(NSString *)label) {
+  [MobClick event:eventId label:label];
+}
+
+RCT_EXPORT_METHOD(onEventWithParameters:(NSString *)eventId attributes:(NSDictionary *)attributes) {
+  [MobClick event:eventId attributes:attributes];
+}
+
+RCT_EXPORT_METHOD(onPageBegin:(NSString *)pageName) {
+  [MobClick beginLogPageView:pageName];
+}
+
+RCT_EXPORT_METHOD(onPageEnd:(NSString *)pageName) {
+  [MobClick endLogPageView:pageName];
+}
+
+RCT_EXPORT_METHOD(setDebugMode:(BOOL)value) {
+  [MobClick setLogEnabled:value];
+}
 
 @end
-  
